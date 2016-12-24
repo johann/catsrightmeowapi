2191a819f0497402f84ae17514ce3ba45cf11347
Johann Kerr <johannkerr@gmail.com>
2016-12-23 22:13:47 -0500

    finalizing catsapi

--- Sources/App/Controllers/CatsController.swift
+++ Sources/App/Controllers/CatsController.swift
@@ -15,13 +15,10 @@ final class CatsController {
     let url = "https://www.reddit.com/r/catgifs/hot.json?limit=100"
     func addRoutes(drop:Droplet) {
         drop.get("") { request in
-            log.verbose("DEFAULT")
-            
             return try drop.view.make("cat")
         }
         drop.get("videos",String.self) { request, after in
-             return try drop.view.make("cat")
-           // return try self.getMoreVideos(request: request, after: after)
+            return try self.getMoreVideos(request: request, after: after)
         }
         drop.get("videos", handler:getVideos)
         drop.get("gifs", handler:getGifs)
@@ -33,11 +30,7 @@ final class CatsController {
         let response = try drop.client.get(url)
         var catArray = [CatVideo]()
         let next = response.data["data","after"]?.string ?? ""
-        
-        
         let linkArray = response.data["data", "children", "data"]?.array?.flatMap({$0.object}) ?? []
-        
-        
         
         for link in linkArray {
             if let title = link["title"]?.string, let url = link["url"]?.string, let thumbnail = link["thumbnail"]?.string {
@@ -97,60 +90,45 @@ final class CatsController {
         
     }
     func getVideos(request: Request) throws -> ResponseRepresentable {
-       log.verbose("getting videos")
-        do {
-            let response = try drop.client.get(url)
-            
-            log.verbose("not going")
-            var catArray = [CatVideo]()
-            let next = response.data["data","after"]?.string ?? ""
-            log.verbose("Got a response")
-            log.verbose("\(next)")
-            
-            let linkArray = response.data["data", "children", "data"]?.array?.flatMap({$0.object}) ?? []
-            log.verbose("\(linkArray.count)")
-            
-            for link in linkArray {
-                if let title = link["title"]?.string, let url = link["url"]?.string, let thumbnail = link["thumbnail"]?.string {
-                    var strippedTitle = title
-                    log.verbose("before strip end")
-                    strippedTitle.stripEnd()
-                    log.verbose("after strip end")
-                    
-                    log.verbose("before range")
-                    if url.range(of: "tumblr") == nil {
-                        log.verbose("after range")
-                        let urlCopy: String
-                        if url.hasSuffix("gif") {
-                            urlCopy = url.replacingOccurrences(of: "gif", with: "mp4")
-                            let catVideo = CatVideo(url: urlCopy, thumbnail: thumbnail, title: strippedTitle)
-                            catArray.append(catVideo)
-                        } else if url.hasSuffix("gifv") {
-                            urlCopy = url.replacingOccurrences(of: "gifv", with: "mp4")
-                            let catVideo = CatVideo(url: urlCopy, thumbnail: thumbnail, title: strippedTitle)
-                            catArray.append(catVideo)
-                        }
-                        
+        let response = try drop.client.get(url)
+        var catArray = [CatVideo]()
+        let next = response.data["data","after"]?.string ?? ""
+        
+        let linkArray = response.data["data", "children", "data"]?.array?.flatMap({$0.object}) ?? []
+        log.verbose("\(linkArray.count)")
+        
+        for link in linkArray {
+            if let title = link["title"]?.string, let url = link["url"]?.string, let thumbnail = link["thumbnail"]?.string {
+                var strippedTitle = title
+                
+                strippedTitle.stripEnd()
+                if url.range(of: "tumblr") == nil {
+                    let urlCopy: String
+                    if url.hasSuffix("gif") {
+                        urlCopy = url.replacingOccurrences(of: "gif", with: "mp4")
+                        let catVideo = CatVideo(url: urlCopy, thumbnail: thumbnail, title: strippedTitle)
+                        catArray.append(catVideo)
+                    } else if url.hasSuffix("gifv") {
+                        urlCopy = url.replacingOccurrences(of: "gifv", with: "mp4")
+                        let catVideo = CatVideo(url: urlCopy, thumbnail: thumbnail, title: strippedTitle)
+                        catArray.append(catVideo)
                     }
                     
                 }
                 
             }
             
-            
-            
-            return try JSON(node:["cats":catArray.makeNode(),
-                                  "next":next])
-        } catch let e as Error {
-            
-            return try JSON(node:["error":"\(e)"])
-           
         }
         
         
-        return Abort.badRequest as! ResponseRepresentable
         
-       
+        return try JSON(node:["cats":catArray.makeNode(),
+                              "next":next])
+        
+        
+        
+        
+        
     }
     func getMoreVideos(request: Request, after: String) throws -> ResponseRepresentable {
         
